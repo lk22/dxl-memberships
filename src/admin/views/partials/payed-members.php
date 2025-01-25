8b67f3b9cefcdc8d7239ca380dd6fa34ae47d473
Leo Knudsen <lk22>
2025-01-25 12:51:55 +0100

    refactoring memberlists in dashbord

--- src/admin/views/partials/payed-members.php
+++ src/admin/views/partials/payed-members.php
@@ -4,32 +4,28 @@
         <table class="widefat fixed striped">
             <thead>
                 <tr>
-                    <th>Navn</th>
-                    <th>Medlemsnummer</th>
                     <th>E-mail</th>
-                    <th>Medlemsskab</th>
-                    <th>Betalings status</th>
-                    <th>Profil status</th>
+                    <th>Køn</th>
                 </tr>
             </thead>
             <tbody>
                 <?php 
                     foreach($members as $member) {
-                        $membership = $wpdb->get_row(
-                            $wpdb->prepare(
-                                "SELECT name from " . $wpdb->prefix . "memberships WHERE id = %d",
-                                $member->membership
-                            )
-                        );
+                        // $membership = $wpdb->get_row(
+                        //     $wpdb->prepare(
+                        //         "SELECT name from " . $wpdb->prefix . "memberships WHERE id = %d",
+                        //         $member->membership
+                        //     )
+                        // );
 
-                        $is_payed = ($member->is_payed == 1) ? "Betalt kontingent" : "Mangler betaling";
-                        $profile_activated = ($member->profile_activated == 1) ? "Aktiveret" : "Ikke aktiv";
+                        // $is_payed = ($member->is_payed == 1) ? "Betalt kontingent" : "Mangler betaling";
+                        // $profile_activated = ($member->profile_activated == 1) ? "Aktiveret" : "Ikke aktiv";
 
                         ?>
                             <tr data-member="<?php echo $member->id ?>">
                                 <td>
                                     <a href="<?php echo generate_dxl_subpage_url(['action' => 'details', 'id' => $member->id]); ?>">
-                                        <?php echo $member->name; ?>
+                                        <?php echo $member->email; ?>
                                     </a>
                                     <div class="actions hidden">
                                         <span class="edit">
@@ -38,24 +34,8 @@
                                     </div>
                                 </td>
                                 <td>
-                                    <?php echo $member->member_number; ?>
+                                    <?php echo $member->gender; ?>
                                 </td>
-                                <td>
-                                    <?php echo $member->email; ?>
-                                </td>
-                                
-                                <td>
-                                    <?php echo ($membership) ? $membership->name : ""; ?>
-                                </td>
-
-                                <td>
-                                    <?php echo $is_payed; ?>
-                                </td>
-
-                                <td>
-                                    <?php echo $profile_activated; ?>
-                                </td>
-                                
                             </tr>
                         <?php
                     }
--- src/admin/views/partials/pending-members.php
+++ src/admin/views/partials/pending-members.php
@@ -5,22 +5,19 @@
             <h1>Afventende medlemmer <small>(<?php echo count($notPayedMembers); ?>)</small></h1><br>
             <table class="widefat fixed striped pending-members-list">
                 <thead>
-                    <th>Navn</th>
-                    <th>Medlemsnummer</th>
                     <th>Email</th>
-                    <th>Afventer godkendelse</th>
-                    <th></th>
+                    <th>Køn</th>
                     <th></th>
                 </thead>
                 <tbody>
                     <?php 
                         foreach($notPayedMembers as $member) {
-                            $awaiting = ($member->is_pending == 1) ? "Afventer" : "Afventer ikke";
+                            // $awaiting = ($member->is_pending == 1) ? "Afventer" : "Afventer ikke";
                             ?>
                                 <tr data-member="<?php echo $member->id ?>">
                                     <td>
                                         <a href="<?php echo generate_dxl_subpage_url(['action' => 'details', 'id' => $member->id]); ?>">
-                                            <?php echo $member->name; ?>
+                                            <?php echo $member->email; ?>
                                         </a>
                                         <div class="actions hidden">
                                             <span class="edit">
@@ -29,13 +26,7 @@
                                         </div>
                                     </td>
                                     <td>
-                                        <?php echo $member->member_number; ?>
-                                    </td>
-                                    <td>
-                                        <?php echo $member->email; ?>
-                                    </td>
-                                    <td>
-                                        <?php echo $awaiting; ?>
+                                        <?php echo $member->gender; ?>
                                     </td>
                                     <td>
                                         <button class="button-primary delete-member-button" data-name="<?php $member->name; ?>" data-member="<?php echo $member->id ?>">Fjern medlem</button>
